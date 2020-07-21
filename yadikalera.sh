dfb5c7ab683f698d45a938ee07c33c2a5ffeab0f
yadikalera <53656254+yadikalera@users.noreply.github.com>
2020-07-21 18:34:50 +0700

    Add files via upload

---
+++ yadikalera.sh
@@ -0,0 +1,24 @@
+# !/bins/bash
+
+clear
+echo "Tool Ini Di gunakan Dengan Bijak"
+echo "[+] DEFACE EROR SYTEM 404 [+] "
+
+
+echo "=========================================="
+
+echo "|••••••TOOLS DDOS ATTACK••••••|"
+
+echo "=========================================="
+echo " [+] Youtube MH TORID [+] "
+echo " [+] Group•deface eror sytem 404 [+] "
+echo " [+] FB•Yadi Sindy [+] "
+echo "=========================================="
+
+read -p"masukin ip target:  "  ip
+read -p"masukin port:  "  port
+read -p"masukin jumlah paket:  " paket
+
+while  [ [ true ]] ;  do
+echo "mengirim paket $paket ke ip $ip port $port"
+done
